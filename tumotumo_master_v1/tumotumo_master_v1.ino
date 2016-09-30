84ba93ca9d9c55474991bc9fb6f76f5505d744cc
Yatoigawa <flandrescarlet823@gmail.com>
2016-10-01 01:34:10 +0900

    コメントアウトの追記
    4thボードが完成していることのコメントアウトを追記

--- tumotumo_master_v1/tumotumo_master_v1.ino
+++ tumotumo_master_v1/tumotumo_master_v1.ino
@@ -256,6 +256,8 @@ void loop() {
           }
 
           //*** 右スティック 半径が閾値以上なら ***
+          // to 4th board
+          // 動作確認済み
           if (R_radius > THRESHOLD_100 && R_STICK_EFFECTIVE == 1) {
             if ( 45 <= R_deg && R_deg < 135) {
               requestMotor(B1010, B0110);
