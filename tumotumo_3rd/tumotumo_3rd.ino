40ea8e04632a4a64cca70e50885e51cb09c4ae0f
Yatoigawa <flandrescarlet823@gmail.com>
2016-10-10 11:25:06 +0900

    特殊処理に対する条件を追記

--- tumotumo_3rd/tumotumo_3rd.ino
+++ tumotumo_3rd/tumotumo_3rd.ino
@@ -18,7 +18,7 @@
 
 #define SERIAL_SPEED  115200
 #define DEVICE_NO	5/*	サブボードの識別番号(データ処理部No.)を入力	*/
-#define UNIT_MAX	4	/*	ドライバユニットの個数						*/
+#define UNIT_MAX	2	/*	ドライバユニットの個数						*/
 #define PARALLEL	A4	/*	パラレル通信用のピン番号を入力(Cピン)		*/
 #define CHECK_LED	A0  /*	信号確認用のＬＥＤのピン番号を入力(Cピン)	*/
 #define VALVE_PIN0	2  //ピンD2
@@ -26,18 +26,13 @@
 #define VALVE_PIN2	4  //ピンD4
 #define VALVE_PIN3	5  //ピンD5
 
-uint8_t data = 0, deviceNo, valveBool, serial_buffer;
+uint8_t data = 0, deviceNo, valveBool, serial_buffer, exception;
 
 const uint8_t output_pins[] = {VALVE_PIN0, VALVE_PIN1, VALVE_PIN2, VALVE_PIN3};
 
 int i;
 
 void setup() {
-  /*
-    DDRD = 0x0F;
-    DDRC = 0x20;
-    DDRB = 0x00;
-  */
   pinMode(VALVE_PIN0, OUTPUT);
   pinMode(VALVE_PIN1, OUTPUT);
   pinMode(VALVE_PIN2, OUTPUT);
@@ -45,49 +40,53 @@ void setup() {
   pinMode(PARALLEL, OUTPUT);
   pinMode(CHECK_LED, OUTPUT);
 
-
   //電磁弁の出力とパラレル通信するところがHIGH、あとはLOW
   //sei();
   Serial.begin(SERIAL_SPEED);  //ボーレート
 
-  while (Serial.available() == 0);	/*シリアル通信が来るまで待つ*/
+  //while (Serial.available() == 0);	/*シリアル通信が来るまで待つ*/
   digitalWrite(CHECK_LED, HIGH);	/*通信できてることを返す*/
-  digitalWrite(PARALLEL, HIGH);
+  //digitalWrite(PARALLEL, HIGH);
 }
 
 void loop() {
   //シリアル通信受信
-  if (Serial.available() != 0) {
+  if (Serial.available() > 0) {
     data = Serial.read();
   }
 
   deviceNo = (data >> 4);
   valveBool = (data & 0b00000001);
+  exception = (data & 0b00001111);
 
   //自分の番号か確認
   if ((deviceNo >= DEVICE_NO) && (deviceNo < (DEVICE_NO + UNIT_MAX))) {
     //処理開始
-    if (valveBool) {
+    if (valveBool == 0b0001) {
       digitalWrite(output_pins[deviceNo - DEVICE_NO], HIGH);
     } else {
       digitalWrite(output_pins[deviceNo - DEVICE_NO], LOW);
     }
   }
+  
   else if (deviceNo == 0b1111) {  // 特殊処理
-    if (valveBool == 0b1000) {
+    if (exception == 0b1000) {
       // シリアル停止処理
       // ないと多分バッファエラー起こすので回避するため記述
       Serial.end();
       digitalWrite(CHECK_LED, LOW);
       // モータ回転開始用停止
-      delay(1000);
-      for (i = 0; i < UNIT_MAX; i++) {
-        digitalWrite(output_pins[i], HIGH);
-      }
+      delay(2000);
+      // デバイス6
+      digitalWrite(output_pins[1], HIGH);
       // モータ側と非同期でシリアル再開
+      delay(1000);
+      digitalWrite(output_pins[1], LOW);
       delay(500);
       Serial.begin(SERIAL_SPEED);
       digitalWrite(CHECK_LED, HIGH);
+      
+      data = 0;
     }
   }
 
